401c1ad444ed18595318943358f0758b152e73ce
kamolehobo <kamolehobo@gmail.com>
2026-04-15 00:33:35 +0200

    Added cell validation and registration logic

--- src/main/java/com/mycompany/poe/Login.java
+++ src/main/java/com/mycompany/poe/Login.java
@@ -1,6 +1,6 @@
 package com.mycompany.poe;
 
-import java.util.regex.Pattern;
+import java.util.regex.Pattern; 
 
 public class Login {
 
@@ -18,4 +18,31 @@ public class Login {
         String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
         return Pattern.matches(regex, password);
     }
+    public boolean checkCellPhoneNumber(String number) {
+    String regex = "^\\+27\\d{9}$";
+    return Pattern.matches(regex, number);
+}
+
+public String registerUser(String username, String password, String cell, String firstName, String lastName) {
+
+    if (!checkUserName(username)) {
+        return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+    }
+
+    if (!checkPasswordComplexity(password)) {
+        return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+    }
+
+    if (!checkCellPhoneNumber(cell)) {
+        return "Cell phone number incorrectly formatted or does not contain international code.";
+    }
+
+    this.username = username;
+    this.password = password;
+    this.cellPhoneNumber = cell;
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    return "User successfully registered.";
+}
 }
