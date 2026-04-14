310695ce6e237576c9939bfcefa18194ae7caf10
kamolehobo <kamolehobo@gmail.com>
2026-04-15 00:27:46 +0200

    Added password complexitity validation

--- src/main/java/com/mycompany/poe/Login.java
+++ src/main/java/com/mycompany/poe/Login.java
@@ -1,5 +1,7 @@
 package com.mycompany.poe;
 
+import java.util.regex.Pattern;
+
 public class Login {
 
     private String username;
@@ -10,5 +12,10 @@ public class Login {
 
     public boolean checkUserName(String username) {
         return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity(String password) {
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+        return Pattern.matches(regex, password);
     }
 }
